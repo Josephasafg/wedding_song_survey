14e8381c4bf63026598718ecb2708b0a347ec1a8
Asaf Gardin <asaf@interai.co>
2021-05-16 15:35:24 +0300

    Added song class

---
+++ src/components/songs/song/song.ts
@@ -0,0 +1,29 @@
+export interface SongProps {
+    artist: string
+    track: string
+    year: number
+    country: string
+}
+
+export class Song {
+    private _country: string;
+    private _artist: string;
+    private _name: string;
+    private _year: number;
+
+    constructor(artist: string, name: string, year: number, country: string) {
+        this._country = country;
+        this._artist = artist;
+        this._name = name;
+        this._year = year;
+    }
+
+    public toSongProps(): SongProps {
+        return {
+            artist: this._artist,
+            track: this._name,
+            country: this._country,
+            year: this._year
+        }
+    }
+}
