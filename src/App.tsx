1eee3cd77b799600ae44d139b5d020f7e5a8b418
Asaf Gardin <asaf@interai.co>
2021-06-11 17:54:49 +0300

    Added context

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 import './App.css';
 import {Songs} from "./components/songs/songs";
 import {SubmitButton} from "./components/submit-button/submit-button";
+import {SongsAPI} from "./API/api";
+import {Song} from "./models/song";
 
 const HELP_US_TEXT = "בבקשה תעזרו לנו לבחור שיר לשבירת הכוס!";
 
+export const SongSubmissionContext = createContext({
+    id: -1, updateSong: (id: number) => {
+    }
+});
+
 function App() {
+    const [songs, setSongs] = useState<Song[]>([]);
+    const [pickedSong, setPickedSong] = useState(-1)
+
+    const onSubmit = async () => {
+        await SongsAPI.submitChoice(pickedSong)
+    }
+
+    async function fetchSongs() {
+        const songs = await SongsAPI.getSongs();
+        setSongs(songs);
+    }
+
+    useEffect(() => {
+        fetchSongs();
+    }, [])
+
 
     return (
-        <div className="App">
-            <div className="help-us-header">
-                <header>{HELP_US_TEXT}</header>
+        <SongSubmissionContext.Provider value={{updateSong: setPickedSong, id: pickedSong}}>
+            <div className="App">
+                <div className="help-us-header">
+                    <header>{HELP_US_TEXT}</header>
+                </div>
+                <Songs songs={songs}/>
+                <SubmitButton onClick={onSubmit}/>
             </div>
-            <Songs/>
-            <SubmitButton/>
-        </div>
+        </SongSubmissionContext.Provider>
     );
 }
 
