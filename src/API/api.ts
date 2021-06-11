eee8665e8c75309fa8533cd49c177167aad08f9e
Asaf Gardin <asaf@interai.co>
2021-06-11 17:53:44 +0300

    Added SONGS api

--- src/API/api.ts
+++ src/API/api.ts
@@ -1,9 +1,34 @@
 import axios from "axios";
+import {Song} from "../models/song";
 
-const DISCOGS_TOKEN = "KKfUXOscwJVXgpxbmYpgMLUecHINICyAnAZMpnGH";
-const BASE_DISCOGS_URL = "https://api.discogs.com";
+const WEDDING_SURVEY_SERVER = "http://localhost:8080";
 
-export const DISCOGS_API = axios.create({
-    baseURL: BASE_DISCOGS_URL,
-    params: {token: DISCOGS_TOKEN}
+export const SONGS_API = axios.create({
+    baseURL: WEDDING_SURVEY_SERVER,
 });
+
+
+export class SongsAPI {
+    public static async getSongs(): Promise<Song[]> {
+        try {
+            const response = await SONGS_API.get("/get-songs");
+
+            if (response && response.status === 200) {
+                return response.data.songs as Song[];
+            }
+
+        } catch (err) {
+            console.error("Failed to fetch songs", err);
+        }
+
+        return [];
+    }
+
+    public static async submitChoice(songId: number): Promise<void> {
+        try {
+            return await SONGS_API.post(`/song/${songId}/update`);
+        } catch (err) {
+            console.error("Failed to submit song choice", err)
+        }
+    }
+}
