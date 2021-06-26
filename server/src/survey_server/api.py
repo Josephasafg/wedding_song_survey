60f815e7e6b3369c076af13c2f76822fb6bd3046
Asaf Gardin <asaf@interai.co>
2021-06-26 13:43:55 +0300

    Moved logic to controller

--- server/src/survey_server/api.py
+++ server/src/survey_server/api.py
@@ -2,24 +2,18 @@ from typing import List
 
 import firebase_admin
 from fastapi import FastAPI, Depends
-from google.cloud import firestore
-from starlette.responses import Response
+from starlette.requests import Request
 
+from server.src.survey_server.dependecies import get_songs_controller
 from server.src.survey_server.models.song import Song
+from server.src.survey_server.songs_controller.songs_controller import SongsController
 
 
-def quickstart_new_instance():
-    # [START firestore_setup_client_create]
-    from google.cloud import firestore
-
-    # Project ID is determined by the GCLOUD_PROJECT environment variable
-    db = firestore.Client()
-    # [END firestore_setup_client_create]
-
-    return db
-
-
-def authenticate_with_file():
+def authenticate_with_file() -> None:
+    """
+    Initialize the firestore app with the json key
+    :return:
+    """
     firebase_admin.initialize_app()
 
 
@@ -27,22 +21,14 @@ app = FastAPI()
 
 
 @app.get('/songs')
-def get_songs(db: firestore.Client = Depends(quickstart_new_instance)) -> List[Song]:
-    songs = db.collection(u'songs').stream()
-
-    return [Song(**song.to_dict()) for song in songs]
+def get_songs(songs_controller: SongsController = Depends(get_songs_controller)) -> List[Song]:
+    return songs_controller.get_songs()
 
 
 @app.put('/song/{song_id}/update')
 def update_songs(song_id: str,
-                 db: firestore.Client = Depends(quickstart_new_instance)):
-    song_document = db.collection(u'songs').document(song_id)
+                 request: Request,
+                 songs_controller: SongsController = Depends(get_songs_controller)):
+    ip_address = request.client.host
 
-    try:
-        song_document.update({'count': firestore.Increment(1)})
-    except Exception as e:
-        print(f'Failed to update song: {song_id} due to an exception: {e}')
-        return Response(status_code=500)
-
-    print(f'Updated song: {song_id}!')
-    return Response(status_code=200)
+    songs_controller.vote(song_id=song_id, ip_address=ip_address)
