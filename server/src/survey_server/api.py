5c76308e0f517907e472d85813e702b7c72ee2f5
Asaf Gardin <asaf@interai.co>
2021-06-26 11:40:04 +0300

    Moved auth call to once

--- server/src/survey_server/api.py
+++ server/src/survey_server/api.py
@@ -3,6 +3,7 @@ from typing import List
 import firebase_admin
 from fastapi import FastAPI, Depends
 from google.cloud import firestore
+from starlette.responses import Response
 
 from server.src.survey_server.models.song import Song
 
@@ -20,14 +21,28 @@ def quickstart_new_instance():
 
 def authenticate_with_file():
     firebase_admin.initialize_app()
-    yield
 
 
 app = FastAPI()
 
 
-@app.get('/songs', dependencies=[Depends(authenticate_with_file)])
+@app.get('/songs')
 def get_songs(db: firestore.Client = Depends(quickstart_new_instance)) -> List[Song]:
     songs = db.collection(u'songs').stream()
 
     return [Song(**song.to_dict()) for song in songs]
+
+
+@app.put('/song/{song_id}/update')
+def update_songs(song_id: str,
+                 db: firestore.Client = Depends(quickstart_new_instance)):
+    song_document = db.collection(u'songs').document(song_id)
+
+    try:
+        song_document.update({'count': firestore.Increment(1)})
+    except Exception as e:
+        print(f'Failed to update song: {song_id} due to an exception: {e}')
+        return Response(status_code=500)
+
+    print(f'Updated song: {song_id}!')
+    return Response(status_code=200)
